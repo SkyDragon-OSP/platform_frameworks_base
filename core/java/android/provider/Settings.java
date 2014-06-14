8eb6e5a64d6813ed2ab97306ee2d1c231af877e4
GeyerA <calo.geyer@gmail.com>
2014-06-14 23:57:26 +0200

    Clock: Second Clock from Provision Team (1/2)

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -2714,6 +2714,12 @@ public final class Settings {
          */
         public static final String STATUSBAR_CLOCK_DATE_FORMAT = "statusbar_clock_date_format";
 
+        /** 
+         * Display second in the Clock
+         * @hide
+         */
+        public static final String CLOCK_USE_SECOND = "clock_use_second";
+
         /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
