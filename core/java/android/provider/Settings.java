6d1a73a4a6d89503336a36d9337463f06b3d7eec
rascarlo <rascarlo@gmail.com>
2014-11-12 10:50:25 +0100

    status bar: native battery percentage
    Conflicts:
    	core/java/android/provider/Settings.java

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -2652,6 +2652,12 @@ public final class Settings {
          */
         public static final String RECENTS_CLEAR_ALL_LOCATION = "recents_clear_all_location";
 
+	/**
+         * Whether to show/hide status bar native battery percentage
+         * @hide
+         */
+        public static final String STATUS_BAR_NATIVE_BATTERY_PERCENTAGE = "status_bar_native_battery_percentage";
+
         /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
