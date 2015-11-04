f2778d7560fd88d1bfba2c3dbd87a15d12bf16c2
maxwen <max.weninger@windriver.com>
2015-11-04 15:20:21 +0100

    [1/2] base: allow to swap volume buttons rotation based
    volume key routing is different then in LP
    
    Change-Id: I190bf07fd89fa1688244a399df731cf33af06f43

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3563,6 +3563,12 @@ public final class Settings {
         public static final String DISABLE_SUGGESTIONS = "disable_suggestions";
 
         /**
+         * Change volume up and down handlign based on rotation
+         * @hide
+         */
+        public static final String SWAP_VOLUME_BUTTONS = "swap_volume_buttons";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
