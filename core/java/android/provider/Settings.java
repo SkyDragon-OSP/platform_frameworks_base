dacb3e3c94de23dec27b795f1de0052b011aff42
HolyAngel <slverwolf@gmail.com>
2016-10-21 09:45:24 -0500

    Revert "SystemUI: Double tap to sleep anywhere on the lock screen (1/2)"
    This reverts commit ae62d159dd87fd28d068f29d6fb10c3618ba0a13.

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3679,12 +3679,6 @@ public final class Settings {
         public static final String SHOW_ALARM_FULLSCREEN = "show_alarm_fullscreen";
 
         /**
-         * Enable double tap gesture anywhere on the lock screen put device to sleep
-         * @hide
-         */
-        public static final String DOUBLE_TAP_SLEEP_ANYWHERE = "double_tap_sleep_anywhere";        /**
-
-        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
