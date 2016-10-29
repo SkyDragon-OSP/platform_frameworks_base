2460a5474e174ca6ca0f783a012d59152f11ad95
HolyAngel <slverwolf@gmail.com>
2016-10-29 14:48:39 -0500

    Revert "Suppress String resources build warnings"
    This reverts commit d864aeddcf6611ce804810cc61b228fbca98ce9b.

--- tools/aapt/ResourceTable.cpp
+++ tools/aapt/ResourceTable.cpp
@@ -2813,7 +2813,6 @@ ResourceTable::validateLocalizations(void)
     for (const auto& nameIter : mLocalizations) {
         const std::map<String8, SourcePos>& configSrcMap = nameIter.second;
 
-#ifdef SHOW_DEFAULT_TRANSLATION_WARNINGS
         // Look for strings with no default localization
         if (configSrcMap.count(defaultLocale) == 0) {
             SourcePos().warning("string '%s' has no default translation.",
@@ -2825,8 +2824,7 @@ ResourceTable::validateLocalizations(void)
             }
             // !!! TODO: throw an error here in some circumstances
         }
-#endif
-#ifdef SHOW_LOCALIZATION_WARNINGS
+
         // Check that all requested localizations are present for this string
         if (mBundle->getConfigurations().size() > 0 && mBundle->getRequireLocalization()) {
             const char* allConfigs = mBundle->getConfigurations().string();
@@ -2876,7 +2874,6 @@ ResourceTable::validateLocalizations(void)
                         configStr.string());
             }
         }
-#endif
     }
 
     return err;
